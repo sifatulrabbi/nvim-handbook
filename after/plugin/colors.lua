f04efe4c876c2edef0626cadc03b5b6a8e4e96b5
Md Sifatul Islam Rabbi <mdsifatulislam.rabbi@gmail.com>
2023-11-28 10:37:29 +0600

    add transparency

---
+++ after/plugin/colors.lua
@@ -0,0 +1,15 @@
+require("github-theme").setup({
+  options = {
+    transparent = true,
+  },
+})
+
+function ColorMyPencils(color)
+  color = color or "github_dark_dimmed"
+  vim.cmd.colorscheme(color)
+
+  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
+
+ColorMyPencils()
