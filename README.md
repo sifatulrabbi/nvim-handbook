137c737b817ba5f364cb871153e273c8326e55f4
Md Sifatul Islam Rabbi <mdsifatulislam.rabbi@gmail.com>
2023-12-04 21:26:56 +0600

    new nvim config :)

--- README.md
+++
@@ -1,9 +0,0 @@
-# My Neovim configuration
-
-### Fetures
-
-- LSP, code suggestions, code diagnostics
-- File searching and file mangement
-- Github color theme
-- A few github diff visualizations
-- Essesntial keybindings
--- after/plugin/fugitive.lua
+++
@@ -1,8 +0,0 @@
-vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = "Open git fugitive" })
-vim.keymap.set("n", "<leader>pp", ':Git push<CR>',
-  {
-    noremap = true,
-    silent = true,
-    desc = "Push changes to the git repo",
-  }
-)
